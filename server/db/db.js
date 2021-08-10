52fface8b323ea3c834738c5078ddf68af03b795
lvmbdv1 <github@lvmbdv.com>
2021-08-09 17:24:27 -0700

    Added initial template

---
+++ server/db/db.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+  logging: false
+});
+
+module.exports = db;
